6cc13e5642598200194a8c8ebf3cd2a963f86b90
Luciana Bahia Wakabayashi Fabri <lucianawakabayashi@gmail.com>
2025-02-05 10:58:15 -0300

    path novo

--- utils/images.py
+++ utils/images.py
@@ -2,19 +2,27 @@
 import os
 from PIL import Image
 import customtkinter as ctk
+import sys
+
+def get_base_path():
+    if getattr(sys, 'frozen', False):  # Executando como .exe via PyInstaller
+        return sys._MEIPASS
+    return os.path.dirname(os.path.abspath(__file__))  # utils/
+
+
+
 
 def load_icons():
     """
     Carrega as imagens necessárias e retorna um dicionário
     com as instâncias de CTkImage.
     """
-    # Ajuste estes caminhos conforme sua estrutura de pastas
-    base_path = os.path.join("assets", "img")
-
+    # Subir um nível para acessar o diretório raiz (sisatas)
+    base_path = os.path.dirname(get_base_path())  # Agora base_path aponta para "sisatas"
 
 
     # Abra as imagens com PIL.Image.open(...)
-    caminho_buscar = os.path.join(base_path, "Buscar Fala.png")
+    caminho_buscar = os.path.join(base_path, "assets", "img", "Buscar_Fala.png")
     imagem_buscar = Image.open(caminho_buscar)
 
     icon_buscar = ctk.CTkImage(
@@ -23,7 +31,7 @@ def load_icons():
         size=(20, 20)
     )
 
-    caminho_atas = os.path.join(base_path, "Listar Atas.png")
+    caminho_atas = os.path.join(base_path, "assets", "img", "Listar_Atas.png")
     imagem_atas = Image.open(caminho_atas)
 
     icon_atas = ctk.CTkImage(
@@ -32,7 +40,7 @@ def load_icons():
         size=(20, 20)
     )
 
-    caminho_secretarias = os.path.join(base_path, "Secretarias.png")
+    caminho_secretarias = os.path.join(base_path, "assets", "img", "Secretarias.png")
     imagem_secretarias = Image.open(caminho_secretarias)
 
     icon_secretarias = ctk.CTkImage(
@@ -41,7 +49,7 @@ def load_icons():
         size=(20, 20)
     )
 
-    caminho_secretarios = os.path.join(base_path, "Secretarios.png")
+    caminho_secretarios = os.path.join(base_path,"assets", "img",  "Secretarios.png")
     imagem_secretarios = Image.open(caminho_secretarios)
 
     icon_secretarios = ctk.CTkImage(
@@ -50,7 +58,7 @@ def load_icons():
         size=(20, 20)
     )
 
-    caminho_importar_dados = os.path.join(base_path, "Importar Dados.png")
+    caminho_importar_dados = os.path.join(base_path,"assets", "img",  "Importar_Dados.png")
     imagem_importar_dados = Image.open(caminho_importar_dados)
 
     icon_importar = ctk.CTkImage(
@@ -59,7 +67,7 @@ def load_icons():
         size=(20, 20)
     )
 
-    caminho_exportar_dados = os.path.join(base_path, "Exportar Dados.png")
+    caminho_exportar_dados = os.path.join(base_path, "assets", "img", "Exportar_Dados.png")
     imagem_exportar_dados = Image.open(caminho_exportar_dados)
  
     icon_exportar = ctk.CTkImage(
@@ -68,7 +76,7 @@ def load_icons():
         size=(20, 20)
     )
 
-    caminho_logo_principal= os.path.join(base_path, "logo_principal.png")
+    caminho_logo_principal= os.path.join(base_path,"assets", "img",  "logo_principal.png")
     imagem_logo_principal = Image.open(caminho_logo_principal)
  
     icon_logo_principal = ctk.CTkImage(
@@ -77,7 +85,7 @@ def load_icons():
         size=(265, 80)
     )
 
-    caminho_adicionar= os.path.join(base_path, "Adicionar.png")
+    caminho_adicionar= os.path.join(base_path,"assets", "img", "Adicionar.png")
     imagem_adicionar = Image.open(caminho_adicionar)
  
     icon_adicionar = ctk.CTkImage(
@@ -86,7 +94,7 @@ def load_icons():
         size=(14, 14)
     )
 
-    caminho_calendar = os.path.join(base_path, "calendar.png")
+    caminho_calendar = os.path.join(base_path,"assets", "img", "calendar.png")
     imagem_calendar = Image.open(caminho_calendar)
 
     icon_calendar = ctk.CTkImage(
@@ -95,7 +103,7 @@ def load_icons():
         size=(14, 14)
     )
 
-    caminho_seta_para_baixo = os.path.join(base_path, "seta_para_baixo.png")
+    caminho_seta_para_baixo = os.path.join(base_path,"assets", "img","seta_para_baixo.png")
     imagem_seta_para_baixo = Image.open(caminho_seta_para_baixo)
 
     icon_seta_para_baixo = ctk.CTkImage(
